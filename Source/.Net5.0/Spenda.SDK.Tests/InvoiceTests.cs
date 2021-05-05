26516156b6620aa5429f10adb193430c3fcafcfa
John Valonda <john.valonda@cirralto.com.au>
2021-05-05 08:22:58 +0800

    added SearchInvoices and helpful comments

--- Source/.Net5.0/Spenda.SDK.Tests/InvoiceTests.cs
+++ Source/.Net5.0/Spenda.SDK.Tests/InvoiceTests.cs
@@ -4,45 +4,118 @@ using RestSharp;
 using Spenda.SDK.Models;
 using System.Diagnostics;
 using System.Collections.Generic;
-
+using System.Linq;
+using System;
 
 namespace Spenda.SDK.Tests
 {
     [TestClass()]
     public class InvoiceTests : BaseTests
     {
+        /// <summary>
+        /// Call the 
+        /// <api>
+        /// GET /api/Invoice 
+        /// </api>
+        /// API to search all of the Invoice.
+        /// </summary>
+        /// <param name="maxNoOfRecords"></param>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <param name="searchString"></param>
+        /// <param name="IsApprovedForPOS"></param>
+        /// <param name="isExactMatch"></param>
+        /// <returns></returns>
+        public List<InvoiceT> SearchInvoices(string searchString=null,
+                                          DateTime? startDate=null,
+                                          DateTime? endDate=null,
+                                          bool isExactMatch=false,
+                                          bool IsApprovedForPOS=false,
+                                          int maxNoOfRecords = 10)
+        {
+            var request = new RestRequest("/api/Invoice");
+
+            //Required parameters
+            request.AddParameter("filter.maxResults", maxNoOfRecords);
+
+            //Optional parameters
+            request.AddParameter("filter.searchString", searchString); 
+            request.AddParameter("filter.isExactMatch", isExactMatch);
+
+            var response = Get<PagedActionResultsOfBusTransSearchResultsT>(request);
+
+            AssertSuccess(response.Messages, response.IsSuccess);
+            
+            return response.Value;
+        }
+        
         [TestMethod()]
         public void InvoiceTest()
         {
             Assert.Fail();
         }
-
+        /// <summary>
+        /// Call the 
+        /// <api>
+        /// GET /api/Invoice
+        /// </api>
+        /// API to get all of the Invoices.
+        /// </summary>
         [TestMethod()]
         public void GetAllInvoiceTest()
         {
             var request = new RestRequest("/api/Invoice");
+
+            //Added filter.maxResults to get 10 records only
             request.AddParameter("filter.maxResults", 10);
 
             var obj = Get<PagedActionResultsOfBusTransSearchResultsT>(request);
 
             AssertSuccess(obj.Messages, obj.IsSuccess);
 
+            //loop through to display the records
             foreach (var invoice in obj.Value)
             {
                 Trace.WriteLine($"Invoice Id: {invoice.ID} , Invoice refNumber: {invoice.RefNumber},  {invoice.Status}");
             }
         }
-
+        /// <summary>
+        /// Call the 
+        /// <api>
+        /// GET /api/Invoice/{id}
+        /// </api>
+        /// API to get a specific Invoice.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        
         [TestMethod()]
         public void GetInvoiceByIdTest()
         {
-            var request = new RestRequest($"/api/Invoice/{488284}");
+            // Get all customers first just so that we can get a valid ID
+            var invoices = SearchInvoices();
+            if (!(invoices?.Any() ?? false)) Assert.Fail("invoices found");
+
+            //select the top invoice ID
+            var id = invoices.FirstOrDefault()?.ID;
+
+            Assert.IsNotNull(id);
+
+            var request = new RestRequest($"/api/Invoice/{id}");
             var obj = Get<TransactionEditResponseOfInvoiceT>(request);
 
             AssertSuccess(obj.Messages, obj.IsSuccess);
             Trace.WriteLine($"Invoice Id: {obj.Value.ID}  Invoice RefNumber: {obj.Value.RefNumber}");
         }
 
+        /// <summary>
+        /// Call the 
+        /// <api>
+        /// GET /api/Invoice/
+        /// </api>
+        /// API to Create an Invoice.
+        /// </summary>
+        /// <returns></returns>
         [TestMethod()]
         public void CreateInvoiceTest()
         {
